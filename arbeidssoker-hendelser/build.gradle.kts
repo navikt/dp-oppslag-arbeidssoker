7d5980461f3689fa9dcdfa3c7211f7c97e4ea662
Geir André Lund <geir.andre.lund@nav.no>
2025-03-18 13:17:51 +0100

    Ta med arbeidssøker-hendelse
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

---
+++ arbeidssoker-hendelser/build.gradle.kts
@@ -0,0 +1,29 @@
+import com.github.davidmc24.gradle.plugin.avro.GenerateAvroProtocolTask
+
+plugins {
+    id("common")
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+}
+
+val schema by configurations.creating {
+    isTransitive = false
+}
+
+dependencies {
+    // Lese hendelser fra arbeidssøkerregistrering
+    implementation("org.apache.avro:avro:1.11.0")
+    schema("no.nav.paw.arbeidssokerregisteret.api:main-avro-schema:1.25.03.10.2-1")
+}
+
+tasks.named("generateAvroProtocol", GenerateAvroProtocolTask::class.java) {
+    source(zipTree(schema.singleFile))
+}
+
+tasks.compileKotlin {
+    dependsOn("generateAvroProtocol")
+}
