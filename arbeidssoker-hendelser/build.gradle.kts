88f2a5c282a7b00124f73a953bdc489e3c90acd9
Geir André Lund <geir.andre.lund@nav.no>
2025-12-09 08:06:43 +0100

    Avro 1.12.1

--- arbeidssoker-hendelser/build.gradle.kts
+++ arbeidssoker-hendelser/build.gradle.kts
@@ -14,7 +14,7 @@ val schema by configurations.creating {
 
 dependencies {
     // Lese hendelser fra arbeidssøkerregistrering
-    api("org.apache.avro:avro:1.12.0")
+    api("org.apache.avro:avro:1.12.1")
     schema("no.nav.paw.arbeidssokerregisteret.api:main-avro-schema:1.13764081353.1-2")
 }
 
